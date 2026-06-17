91bcbc9ca71f88f153bcd1a2efbfb184650f505b
L-R0QU3 <luis.roquetic@gmail.com>
2026-06-16 21:33:20 -0400

    fix: unificar ubicación de dist en frontend/

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -18,6 +17,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist'), // Genera dentro de frontend/
+    outDir: 'dist', // Genera frontend/dist
   },
 });
